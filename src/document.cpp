356de12e56d433871f0b35e0975d49aad1398aeb
coldisice <vladislavpudovkin1@gmail.com>
2025-09-15 18:45:46 +0300

    Улучшение читаемости и работоспособности кода (ввиду изменения заголовочного файла).
    Теперь при обращении к полям класса используется ключевое слово (this->) вместо (Document::).
    
    Изменены функции insertText(), deleteTextInRange() и deleteText() в виду существования альтернативы в виде std::string::erase и insert.
    
    Изменена функция getTextFromFile(), так как до этого она работала неправильно (в результирующий массив не добавлялся символ перехода строки, из-за чего он отображался некорректно с помощью функции showText()).
    
    Функция showText() также была изменена, так как она работала не так, как ожидалось.
    
    Добавлена перегруженная функция getText().

--- src/document.cpp
+++ src/document.cpp
@@ -4,115 +4,130 @@
 
 #include <iostream>
 
-int Document::getInfo(bool ignorecount = 0){
+int Document::getInfo(bool ignorecount) {
     if (!(ignorecount)){
-        int lengthStr=0;
-        for (int i = 0; i < Document::result.size();i++){
-            if (Document::result.at(i)=='\n' || Document::result.at(i)=='\0') {
-                Document::rows++;
-                Document::endStr.push_back(i);
-                Document::lengthStr.push_back(lengthStr);
-                lengthStr=0;
+        int lengthStr = 0;
+        for (int i = 0; i < this->result.size(); i++)  {
+            if (this->result.at(i) == '\n' || this->result.at(i) == '\0') {
+                this->rows++;
+                this->endStr.push_back(i);
+                this->lengthStr.push_back(lengthStr);
+                lengthStr = 0;
             }
             else {
                 lengthStr++;
-                if (Document::result.size()-i==1){
-                    Document::endStr.push_back(i);
-                    Document::lengthStr.push_back(lengthStr);
+                if (this->result.size() - i == 1) {
+                    this->endStr.push_back(i);
+                    this->lengthStr.push_back(lengthStr);
                 }
                 continue;
             }
         }
-
     }
-    return Document::rows;
+    return this->rows;
 }
 
-void Document::showText(){
-    std::cout << Document::result;
+void Document::showText() {
+    std::cout << "\n[SHOW TEXT]\n";
+    for (int i = 0; i < this->result.size(); i++){
+        if (this->result[i] == '\n') std::cout << "\n";
+        else std::cout << this->result[i];
+    }
 }
 
-int Document::findElementRow(int id){
-    for (int i = 0; i<Document::endStr.size();i++){
-        if (id<Document::endStr[i]) return i;
+int Document::findElementRow(int id) {
+    for (int i = 0; i < this->endStr.size(); i++) {
+        if (id < this->endStr[i]) {
+            return i;
+        }
         else continue;
     }
     return -1;
 }
 
-void Document::insertText(int offset, std::string yourText){
-    for (int j = 0; j < yourText.size();j++){
-        Document::buffer = Document::result;
-        Document::result.push_back(' ');
-        for (int i =0;i<(Document::result.size()-offset);i++){
-        Document::result[Document::result.size()-1-i]=Document::buffer[Document::buffer.size()-1-i];
-        }
-        Document::result[offset] = yourText[yourText.size()-1-j];
-    }
-    Document::buffer.clear();
-    Document::endStr.clear();
-    Document::lengthStr.clear();
-    getInfo();
+void Document::insertText(int offset, const std::string* yourText) {
+    this->result.insert(offset, *yourText);
+    this->endStr.clear();
+    this->lengthStr.clear();
+    this->getInfo();
 }
 
-void Document::deleteTextInRange(int left, int right){
-    Document::buffer = Document::result;
-    Document::result = Document::buffer.substr(0,left);
-    Document::result += Document::buffer.substr(right+1,Document::buffer.size());
-    Document::endStr.clear();
-    Document::lengthStr.clear();
-    getInfo();
+void Document::deleteTextInRange(int index, int count) {
+    this->result.erase(index, count);
+    this->endStr.clear();
+    this->lengthStr.clear();
+    this->getInfo();
 }
 
-void Document::deleteText(int offset) {
-    Document::buffer = Document::result;
-    Document::result = Document::buffer.substr(0,offset);
-    Document::result += Document::buffer.substr(offset+1,Document::buffer.size());
-    Document::endStr.clear();
-    Document::lengthStr.clear();
-    getInfo();
+void Document::deleteTextAt(int index) {
+    this->result.erase(index, 1);
+    this->endStr.clear();
+    this->lengthStr.clear();
+    this->getInfo();
 }
 
-void Document::clearTextAtFile(std::string fileName){
-    std::ofstream out(fileName);
+void Document::clearTextAtFile(const std::string* fileName) {
+    std::ofstream out(*fileName);
     out.close();
 }
 
-void Document::clearText(){
-    Document::result.clear();
-    Document::endStr.clear();
-    Document::lengthStr.clear();
-    getInfo();
+void Document::clearText() {
+    this->result.clear();
+    this->endStr.clear();
+    this->lengthStr.clear();
+    this->getInfo();
 }
 
-void Document::getTextFromFile(std::string fileName){
-    std::ifstream in(fileName);
+void Document::getTextFromFile(const std::string* fileName) { // при чтении есть возможность записать конец каждой строки с помощью .back по идее, собственно как и индексы конца строк
+    std::ifstream in(*fileName);
     if (in.is_open()){
-        while (std::getline(in, buffer)){
-            Document::buffer="";
-            Document::result+=Document::buffer;
-            
+        while(std::getline(in, buffer)){
+            this->result += this->buffer;
+            if (buffer[buffer.size() == '\n']) { 
+                this->result.push_back('\n');
+            }
+            this->buffer.clear();
         }
-        Document::result+="\n";
+    }
+    else {
+        std::cout << "\n[ERROR] File not found or you forgot extension .txt in name\n";
     }
     in.close();
-    Document::endStr.clear();
-    Document::lengthStr.clear();
-    getInfo();
+    this->endStr.clear();
+    this->lengthStr.clear();
+    this->getInfo();
 }
 
-void Document::getText(std::string yourText){
-    Document::result+=yourText;
-    Document::endStr.clear();
-    Document::lengthStr.clear();
-    getInfo();
+void Document::getText(const std::string* yourText) {
+    this->result += *yourText;
+    this->endStr.clear();
+    this->lengthStr.clear();
+    this->getInfo();
 }
 
-void Document::textToFile(std::string fileName){
+void Document::textToFile(const std::string* fileName) {
     std::ofstream out;
-    out.open(fileName);
+    out.open(*fileName);
     if (out.is_open()){
-        out << Document::result;
+        out << this->result;
+    }
+    else {
+        std::cout << "\n[ERROR] File not found or you forgot extension .txt in name\n";
     }
     out.close();
+}
+
+
+// перегруженные функции
+
+void Document::getText(const char* yourText) {
+    int* i = {new int {0}};
+    while(*(yourText + *i) != '\0') {
+        this->result += *(yourText + *i);
+        (*i)++;
+    }
+    delete i;
+    this->endStr.clear();
+    this->lengthStr.clear();
+    this->getInfo();
 }
